a65c49bec0b28d3e42247446d9111fbde7a5276e
Hong Anh Pham <phamhonganh12062000@gmail.com>
2025-01-23 14:22:52 +0700

    complete eval infix expr

--- notes/Self-evaluating expressions.md
+++ notes/Self-evaluating expressions.md
@@ -30,3 +30,15 @@ Prefix expressions in this section are just _operator expressions with one opera
 > We are defining the semantics of our language. A small change in the evaluation of operator expressions might cause something unintended in a part of a language that seems unrelated.
 
 We first evaluate its operand and then use the result of this evaluation with the operator
+
+## Infix Expressions
+
+There are two groups of infix expressions: The group that produces boolean values e.g., `5 != 5` and the group that produces other types of values e.g., `5 + 5`
+
+Our language will NOT support boolean arithmetic like adding/subtracting/dividing/multiplying booleans like C/C++ or Python
+
+> [!IMPORTANT]
+> We are doing **pointer comparison** here as our method arguments are _pointers instead of values_, and pointer comparison is much faster since _we are comparing the same instances_.
+
+> [!WARNING]
+> We can use pointer comparison for booleans as operands, but we cannot do so with integer operands since _we are always creating new pointers_. For this reason, `5 == 5` would be false since we are comparing pointers with `object.Object` as value wrapper.
--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -5,8 +5,8 @@ import (
 	"s8/src/object"
 )
 
-// To NOT create new instances of object.Boolean or object.Null
-// We use reference instead
+// To NOT create new instances of object.Boolean or object.Null and use reference instead
+// This improves performance too (pointer comparison is faster than value comparison)
 var (
 	NULL  = &object.Null{}
 	TRUE  = &object.Boolean{Value: true}
@@ -27,6 +27,10 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 	}
 
 	return nil
@@ -42,6 +46,7 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	return result
 }
 
+// Take Go's native true and return singleton TRUE pointer
 func nativeBoolToBooleanObj(input bool) *object.Boolean {
 	if input {
 		return TRUE
@@ -54,13 +59,16 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
 	default:
 		return NULL
 	}
 }
 
-func evalBangOperatorExpression(obj object.Object) object.Object {
-	switch obj {
+func evalBangOperatorExpression(right object.Object) object.Object {
+	// If something has the SAME MEMORY ADDRESS value as boolean constants here, then it works
+	switch right {
 	case TRUE:
 		return FALSE
 	case FALSE:
@@ -69,5 +77,60 @@ func evalBangOperatorExpression(obj object.Object) object.Object {
 		return TRUE
 	default:
 		return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTERGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
+}
+
+func evalInfixExpression(operator string, left, right object.Object) object.Object {
+	switch {
+	// We cannot use pointer comparison here
+	// Since we are always allocating NEW instances of object.Integer
+	case left.Type() == object.INTERGER_OBJ && right.Type() == object.INTERGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+		// For cases like TRUE == TRUE
+		// Here we use POINTER COMPARISON by comparing the memory addresses of two *object.Object pointers
+		// The result is a native Go boolean
+	case operator == "==":
+		return nativeBoolToBooleanObj(left == right)
+	case operator == "!=":
+		return nativeBoolToBooleanObj(left != right)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(operator string, left, right object.Object) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+
+	switch operator {
+	// Group 1: Produce values of other types than booleans
+	case "+":
+		return &object.Integer{Value: leftVal + rightVal}
+	case "-":
+		return &object.Integer{Value: leftVal - rightVal}
+	case "*":
+		return &object.Integer{Value: leftVal * rightVal}
+	case "/":
+		return &object.Integer{Value: leftVal / rightVal}
+	// Group 2: Produce booleans as their results
+	case "<":
+		return nativeBoolToBooleanObj(leftVal < rightVal)
+	case ">":
+		return nativeBoolToBooleanObj(leftVal > rightVal)
+	case "==":
+		return nativeBoolToBooleanObj(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBooleanObj(leftVal != rightVal)
+	default:
+		return NULL
 	}
 }
